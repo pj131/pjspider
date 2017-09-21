f19abd574d88465220decf7a5fb254778a57ff06
pj131 <pjie131@qq.com>
2017-09-21 20:54:42 +0800

    main py

---
+++ pjspider.py
@@ -0,0 +1,21 @@
+# coding=utf-8
+import os
+import shutil
+import platform
+import img
+import beauty360
+
+basePath = r"./360photo"
+if platform.system() == 'Windows':
+    basePath = r"D:/360photo"
+
+if os.path.isdir(basePath):
+    shutil.rmtree(basePath)
+
+if not os.path.isdir(basePath):
+    os.mkdir(basePath)
+
+#for i in xrange(10) :
+#    beauty360.spider360(i*30,basePath)
+
+print 'beauty360 done ......',img.savecount
